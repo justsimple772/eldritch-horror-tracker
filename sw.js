5ae697bd00e3866ac62cb0c9d64c2af185e30429
justsimple772 <cjh940901@gmail.com>
2026-09-14 14:44:05 +0900

    Add scalable fanfare and motion permission fallback

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="eh-tracker-v17";
-const PRECACHE=["./","./index.html","./manifest.webmanifest","./mobile-ui.css?v=20260914-f","./icon-192.png","./icon-512.png","./apple-touch-icon.png"];
+const CACHE="eh-tracker-v18";
+const PRECACHE=["./","./index.html","./manifest.webmanifest","./mobile-ui.css?v=20260914-g","./icon-192.png","./icon-512.png","./apple-touch-icon.png"];
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(PRECACHE)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>/^eh-tracker-v\d+$/.test(k)&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("message",event=>{if(event.data&&event.data.type==="SKIP_WAITING") self.skipWaiting();});
